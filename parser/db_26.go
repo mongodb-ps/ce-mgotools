78664016729eb4487ac19c10e190b8fbb7a4b2a4
jtv4k <jtv4k@users.noreply.github.com>
2018-09-13 13:30:42 -0700

    Fix counter appearing in locks

--- parser/db_26.go
+++ parser/db_26.go
@@ -38,22 +38,28 @@ func (v *Version26Parser) NewLogMessage(entry record.Entry) (record.Message, err
 		switch {
 		case r.ExpectString("command"):
 			return parse26Command(r)
+
 		case r.ExpectString("query"),
 			r.ExpectString("getmore"),
 			r.ExpectString("geonear"),
 			r.ExpectString("insert"),
 			r.ExpectString("update"),
 			r.ExpectString("remove"):
-			// Commands, inserts, and queries!
-			return parse26Operation(r)
-			// Look at things related to index builds.
+
+			m, err := parse26Operation(r)
+			if err != nil {
+				return m, err
+			}
+
+			return normalize26Query(m.(record.MsgCommandLegacy)), nil
+
 		case r.ExpectString("build index on"):
+			// Look at things related to index builds.
 			return parse26BuildIndex(r)
+
 		default:
 			// Check for network status changes.
 			if msg, err := ParseNetwork(r, entry); err == nil {
-				return msg, err
-			} else if msg, err := ParseDDL(r, entry); err == nil {
 				return msg, err
 			}
 		}
@@ -71,10 +77,72 @@ func parse26BuildIndex(r util.RuneReader) (record.Message, error) {
 	// 2.6 index building format is the same as 3.x
 	return parse3XBuildIndex(r)
 }
+
+func normalize26Query(m record.MsgCommandLegacy) record.Message {
+	query := msgHasPayload(m.Payload, "query")
+	comment := func() string {
+		if p := query; p != nil {
+			return p["$comment"].(string)
+		}
+		return ""
+	}()
+
+	switch m.Operation {
+	case "query":
+		comment, _ = query["$comment"].(string)
+
+		c := record.MsgCRUD{
+			Message: m,
+
+			Filter:  query,
+			Sort:    msgHasPayload(m.Payload, "orderby"),
+			Comment: comment,
+			N:       m.Counters["nreturned"],
+		}
+
+		if c.Sort != nil || c.Comment != "" {
+			if f, ok := m.Payload["query"].(map[string]interface{}); ok {
+				c.Filter = f
+			}
+		}
+
+		return c
+
+	case "update":
+		return record.MsgCRUD{
+			Message: m,
+
+			Filter:  query,
+			Comment: comment,
+			N:       m.Counters["mModified"],
+		}
+	case "remove":
+		return record.MsgCRUD{
+			Message: m,
+
+			Filter:  query,
+			Comment: comment,
+			N:       m.Counters["ndeleted"],
+		}
+
+	case "insert":
+		return record.MsgCRUD{
+			Message: m,
+
+			Filter:  query,
+			Comment: comment,
+			N:       m.Counters["ninserted"],
+		}
+
+	default:
+		return m
+	}
+}
+
 func parse26Command(r util.RuneReader) (record.Message, error) {
 	// command test.$cmd command: insert { insert: "foo", documents: [ { _id: ObjectId('59e3fdf50bae7edf962785a7'), a: 1.0 } ], ordered: true } keyUpdates:0 numYields:0 locks(micros) w:159 reslen:40 0ms
 	var err error
-	op := record.MakeMsgOpCommandLegacy()
+	op := record.MakeMsgCommandLegacy()
 	if opn, ok := r.SlurpWord(); ok {
 		op.Operation = opn
 	} else {
@@ -87,7 +155,7 @@ func parse26Command(r util.RuneReader) (record.Message, error) {
 	}
 	// Skip the "command:" portion, since it's irrelevant here.
 	r.SkipWords(1)
-	target := op.Counters
+	locks := false
 	for {
 		if param, ok := r.SlurpWord(); ok {
 			if r.Expect('{') {
@@ -96,26 +164,28 @@ func parse26Command(r util.RuneReader) (record.Message, error) {
 				}
 				op.Command = param
 			} else if param == "locks(micros)" {
-				target = op.Locks
+				locks = true
 				continue
 			} else if strings.HasPrefix(param, "ms") {
 				if op.Duration, err = strconv.ParseInt(param[:len(param)-2], 10, 64); err != nil {
 					return nil, err
 				}
-			} else if !parseIntegerKeyValue(param, target, mongo.COUNTERS) {
+			} else if (locks && !parseIntegerKeyValue(param, op.Locks, map[string]string{"r": "r", "R": "R", "w": "w", "W": "W"})) ||
+				!parseIntegerKeyValue(param, op.Counters, mongo.COUNTERS) {
 				break
 			}
 		}
 	}
-	return NormalizeCommand(op.MsgOpCommandBase), nil
+	return op, nil
 }
+
 func parse26Operation(r util.RuneReader) (record.Message, error) {
 	// getmore test.foo cursorid:30107363235 ntoreturn:3 keyUpdates:0 numYields:0 locks(micros) r:14 nreturned:3 reslen:119 0ms
 	// insert test.foo query: { _id: ObjectId('5a331671de4f2a133f17884b'), a: 2.0 } ninserted:1 keyUpdates:0 numYields:0 locks(micros) w:10 0ms
 	// remove test.foo query: { a: { $gte: 9.0 } } ndeleted:1 keyUpdates:0 numYields:0 locks(micros) w:63 0ms
 	// update test.foo query: { a: { $gte: 8.0 } } update: { $set: { b: 1.0 } } nscanned:9 nscannedObjects:9 nMatched:1 nModified:1 keyUpdates:0 numYields:0 locks(micros) w:135 0ms
 	var err error
-	op := record.MakeMsgOpCommandLegacy()
+	op := record.MakeMsgCommandLegacy()
 	// Grab the operation name first.
 	if opn, ok := r.SlurpWord(); ok {
 		op.Operation = opn
@@ -127,12 +197,12 @@ func parse26Operation(r util.RuneReader) (record.Message, error) {
 	} else {
 		r.RewindSlurpWord()
 	}
-	target := op.Counters
+	locks := false
 	for param, ok := r.SlurpWord(); ok; param, ok = r.SlurpWord() {
 		if strings.HasPrefix(param, "locks") {
 			if param == "locks(micros)" {
 				// Locks follow this param, so reset the target to the locks map.
-				target = op.Locks
+				locks = true
 				continue
 			} else {
 				// Wrong version.
@@ -144,7 +214,7 @@ func parse26Operation(r util.RuneReader) (record.Message, error) {
 				return nil, err
 			}
 			continue
-		} else if length := len(param); length > 1 && util.ArrayBinarySearchString(param[:length-1], mongo.OPERATION_COMMANDS) {
+		} else if length := len(param); length > 1 && util.ArrayBinarySearchString(param[:length-1], mongo.COMMANDS) {
 			if r.EOL() {
 				return nil, VersionErrorUnmatched{"unexpected end of string"}
 			} else if r.Expect('{') {
@@ -158,7 +228,9 @@ func parse26Operation(r util.RuneReader) (record.Message, error) {
 			}
 		} else if strings.ContainsRune(param, ':') {
 			// A counter (in the form of key:value) needs to be applied to the correct target.
-			parseIntegerKeyValue(param, target, mongo.COUNTERS)
+			if locks && !parseIntegerKeyValue(param, op.Locks, map[string]string{"r": "r", "R": "R", "w": "w", "W": "W"}) {
+				parseIntegerKeyValue(param, op.Counters, mongo.COUNTERS)
+			}
 		} else if strings.HasSuffix(param, "ms") {
 			// Found a duration, which is also the last thing in the line.
 			op.Duration, _ = strconv.ParseInt(param[0:len(param)-3], 10, 64)
@@ -172,4 +244,14 @@ func parse26Operation(r util.RuneReader) (record.Message, error) {
 }
 func (v *Version26Parser) Version() VersionDefinition {
 	return VersionDefinition{Major: 2, Minor: 6, Binary: record.BinaryMongod}
+}
+
+func msgHasPayload(msg record.Message, key string) map[string]interface{} {
+	if m, ok := msg.(record.MsgCommandBase); !ok {
+		return nil
+	} else if p, ok := m.Payload[key].(map[string]interface{}); !ok {
+		return nil
+	} else {
+		return p
+	}
 }
