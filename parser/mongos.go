d00709a1ded6abc3c5cf2dd6ffa0e31d2026fb6f
jtv4k <jtv4k@users.noreply.github.com>
2019-05-09 20:42:45 -0500

    Start some long neglected mongos code

--- parser/mongos.go
+++ parser/mongos.go
@@ -14,13 +14,21 @@ func S(entry record.Entry) mongos {
 
 func (entry mongos) Control(r *util.RuneReader) (record.Message, error) {
 	switch entry.Context {
-	case "mongosMain":
-	case "mongoS":
+	case "mongosMain",
+		"mongoS":
 		if r.ExpectString("MongoS version") {
 			return version(r.SkipWords(2).Remainder(), "mongos")
 		} else if r.ExpectString("options:") {
 			return startupOptions(r.SkipWords(1).Remainder())
+		} else if r.ExpectString("build info") {
+			return record.MsgBuildInfo{BuildInfo: r.SkipWords(2).Remainder()}, nil
 		}
 	}
 	return nil, internal.ControlUnrecognized
+}
+
+func (entry mongos) Network(r *util.RuneReader) (record.Message, error) {
+	if entry.Connection > 0 {
+
+	}
 }
--- parser/s_24.go
+++ parser/s_24.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"mgotools/internal"
 	"mgotools/record"
+	"mgotools/util"
 )
 
 type Version24SParser struct{}
@@ -16,12 +17,29 @@ func init() {
 var errorVersion24SUnmatched = internal.VersionUnmatched{"mongos 2.4"}
 
 func (v *Version24SParser) NewLogMessage(entry record.Entry) (record.Message, error) {
+	r := util.NewRuneReader(entry.RawMessage)
+
+	switch {
+	case entry.Context == "mongosMain":
+		if msg, err := S(entry).Control(r); err != nil {
+			return msg, nil
+		} else if r.ExpectString("connection accepted") {
+			if ip, port, conn, ok := connectionInit(r); ok {
+				return record.MsgConnection{
+					Address: ip,
+					Conn:    conn,
+					Port:    port,
+					Opened:  true,
+				}, nil
+			}
+		}
+
+	}
 	return nil, errorVersion24SUnmatched
 }
 
 func (v *Version24SParser) Check(base record.Base) bool {
-	return base.CString &&
-		base.RawSeverity == record.SeverityNone &&
+	return base.RawSeverity == record.SeverityNone &&
 		base.RawComponent == ""
 }
 
